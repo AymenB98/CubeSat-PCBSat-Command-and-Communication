4f1f1ca9252d7874ecfbaaf3f8884a48897de915
AymenB98 <60265156+AymenB98@users.noreply.github.com>
2020-10-27 20:53:40 +0000

    CubeSat code working.
    This code allows for bi-directional communication between two CC1310 boards. The microSD functionality is integrated also.

---
+++ Debug/rfEchoTx.d
@@ -0,0 +1,105 @@
+# FIXED
+
+rfEchoTx.obj: ../rfEchoTx.c
+rfEchoTx.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdlib.h
+rfEchoTx.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_ti_config.h
+rfEchoTx.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/linkage.h
+rfEchoTx.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/cdefs.h
+rfEchoTx.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stddef.h
+rfEchoTx.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdint.h
+rfEchoTx.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_stdint40.h
+rfEchoTx.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/stdint.h
+rfEchoTx.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_types.h
+rfEchoTx.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_types.h
+rfEchoTx.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_stdint.h
+rfEchoTx.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_stdint.h
+rfEchoTx.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/string.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/drivers/rf/RF.h
+rfEchoTx.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdbool.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/drivers/dpl/ClockP.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/drivers/dpl/SemaphoreP.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/drivers/utils/List.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/DeviceFamily.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/rf_common_cmd.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/rf_mailbox.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/rf_prop_cmd.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/rf_ble_cmd.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/drivers/PIN.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/drivers/pin/PINCC26XX.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/drivers/dpl/HwiP.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/ioc.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/../inc/hw_types.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/../inc/../inc/hw_chip_def.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/../inc/hw_memmap.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/../inc/hw_ioc.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/../inc/hw_ints.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/interrupt.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/../inc/hw_nvic.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/debug.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/cpu.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/../inc/hw_cpu_scs.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/../driverlib/rom.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/gpio.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/../inc/hw_gpio.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/drivers/SD.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/display/Display.h
+rfEchoTx.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdarg.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/rf_prop_mailbox.h
+rfEchoTx.obj: ../Board.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/drivers/Board.h
+rfEchoTx.obj: ../CC1310_LAUNCHXL.h
+rfEchoTx.obj: ../RFQueue.h
+rfEchoTx.obj: C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/rf_data_entry.h
+rfEchoTx.obj: ../smartrf_settings/smartrf_settings.h
+
+../rfEchoTx.c: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdlib.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_ti_config.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/linkage.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/cdefs.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stddef.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdint.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_stdint40.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/stdint.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_types.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_types.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_stdint.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_stdint.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/string.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/drivers/rf/RF.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdbool.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/drivers/dpl/ClockP.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/drivers/dpl/SemaphoreP.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/drivers/utils/List.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/DeviceFamily.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/rf_common_cmd.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/rf_mailbox.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/rf_prop_cmd.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/rf_ble_cmd.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/drivers/PIN.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/drivers/pin/PINCC26XX.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/drivers/dpl/HwiP.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/ioc.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/../inc/hw_types.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/../inc/../inc/hw_chip_def.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/../inc/hw_memmap.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/../inc/hw_ioc.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/../inc/hw_ints.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/interrupt.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/../inc/hw_nvic.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/debug.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/cpu.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/../inc/hw_cpu_scs.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/../driverlib/rom.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/gpio.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/../inc/hw_gpio.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/drivers/SD.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/display/Display.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdarg.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/rf_prop_mailbox.h: 
+../Board.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/drivers/Board.h: 
+../CC1310_LAUNCHXL.h: 
+../RFQueue.h: 
+C:/ti/simplelink_cc13x0_sdk_4_10_01_01/source/ti/devices/cc13x0/driverlib/rf_data_entry.h: 
+../smartrf_settings/smartrf_settings.h: 
